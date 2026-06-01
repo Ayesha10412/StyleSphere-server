3db036dc1c0ae50e37eb3ed04746880106d46aff
ayesha10412 <k.bristi057@gmail.com>
2026-06-01 09:08:43 +0600

    updated order route

--- src/app/routes/order.routes.ts
+++ src/app/routes/order.routes.ts
@@ -8,7 +8,7 @@ import { orderController } from "../modules/controller/order.controller";
 const router = Router();
 router.get("/", checkAuth(ROLE.SELLER), orderController.allOrder);
 router.get("/:id", checkAuth(ROLE.SELLER), orderController.orderDetails);
-router.get(
+router.post(
   "/",
   checkAuth(ROLE.USER),
   validateRequest(createOrderValidation),
